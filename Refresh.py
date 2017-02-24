6f0226d3a855221b8c042ff776c7fd22cda4de28
WILL LIU <wrliu@uwaterloo.ca>
2017-02-24 13:29:24 -0500

    Added print statements for current time

--- Refresh.py
+++ Refresh.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import webbrowser
+from datetime import datetime
 
 def make_request(link):
     request = requests.get(link, timeout=2)
@@ -21,6 +22,9 @@ def main():
         section = response.find('p', { 'data-altcopy' : 'product_coming_soon_1'})
         if section is None:
             linkChanged = True
+        else:
+            print(section)
+            print(datetime.now().time())
     open_url(link)
 
 if __name__ == '__main__':main()
